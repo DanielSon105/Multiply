5073442183858e87b5febb3cb6140eb86f4eabcd
DanielSon105 <daniel.g.barrido@gmail.com>
2015-09-28 14:39:31 -0500

    Initial Commit

---
+++ Multiply/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Multiply
+//
+//  Created by Daniel Barrido on 9/28/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Multiply/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Multiply
+//
+//  Created by Daniel Barrido on 9/28/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
